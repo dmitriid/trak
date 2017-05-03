4e55261c117b1cfeb7638ae812b2bdd2ada78b7d
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-05-03 09:07:35 +0200

    Add a basic api module (find albums only so far)

---
+++ src/trak/api.cljs
@@ -0,0 +1,25 @@
+(ns trak.api
+  (:require [trak.utils :as utils]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [trak.pubsub :as pubsub])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+
+(defn- api-call
+  ([method url callback] (api-call method url {} callback))
+  ([method url other callback]
+   (go (let [response (<! (method url other))]
+         (callback response)))
+    ))
+
+
+(defn find-albums [string]
+  (api-call (partial http/get)
+            "https://api.spotify.com/v1/search"
+            {:query-params      {:q string :type "album"}
+             :with-credentials? false}
+            (fn [response] (pubsub/publish :actions {:action :api-call-result
+                                                     :params {:call     :find-albums
+                                                              :response response}})))
+  )
