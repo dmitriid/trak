94fa4646b9142739dc73cf420b9bdf64bc34d8d9
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-05-03 09:08:51 +0200

    Add unpack-entities db helper

--- src/trak/db.cljs
+++ src/trak/db.cljs
@@ -13,3 +13,12 @@
                               }))
 
 
+;
+; If you have a collection of entites (as it's usually
+; returned by datascript queries), unpack them into entities
+; (so we have (1 2 3) instead of #{[1] [2] [3]} etc.)
+;
+(defn unpack-entities [db collection-of-ids]
+  (->> collection-of-ids
+       (map first)
+       (map #(d/entity db %))))
