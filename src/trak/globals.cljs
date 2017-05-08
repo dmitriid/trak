b05bcf0b084f48ce8e295f69745276d497e675e3
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-05-08 18:04:17 +0200

    Spotify auth globals

--- src/trak/globals.cljs
+++ src/trak/globals.cljs
@@ -1,6 +1,7 @@
 (ns trak.globals
   (:require-macros [trak.config :refer [cljs-env]])
-  (:require [trak.utils :as utils])
+  (:require [trak.utils :as utils]
+            [clojure.string :as str])
   (:import goog.Uri
            goog.Uri.QueryData
            goog.string))
@@ -11,16 +12,25 @@
   (utils/info kws)
   (utils/info (.createFromKeysValues QueryData (map name (keys kws)) (vals kws)))
   (reduce #(.add %1
-                 (.urlEncode string (name (first %2)))
-                 (.urlEncode string (second %2)))
+                 (name (first %2))
+                 (second %2))
           (QueryData.)
           kws)
   )
 
 (defn spotify-authorization-link [scope]
   (doto (Uri. "https://accounts.spotify.com/authorize")
-    (.setQuery (build-query-params {:response_type "code"
+    (.setQuery (build-query-params {:response_type "token"
                                     :client_id     (cljs-env :spotify-client-id)
                                     :scope         scope
                                     :redirect_uri  (cljs-env :spotify-redirect-url)
                                     :state         (.getRandomString string)}))))
+
+(defn get-spotify-auth-token []
+  (let [fragment (.getFragment (Uri. (.-href (.-location js/document))))
+        fragment (str/split fragment "&")]
+    (reduce (fn [acc part]
+              (let [kw (str/split part "=")]
+                (merge acc {(keyword (first kw)) (second kw)})))
+            {}
+            fragment)))
