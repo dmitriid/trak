309de95219c6824495d357a31350dae3f9ccf8f0
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-05-15 17:36:31 +0200

    Display tracks when selecting playlists

--- src/trak/ui/app.cljs
+++ src/trak/ui/app.cljs
@@ -6,7 +6,9 @@
             [trak.ui.components :as components]
             [trak.globals :as globals]
             [trak.ui.components.landing :as landing]
-            [trak.ui.components.profile :as profile]))
+            [trak.ui.components.profile :as profile]
+            [trak.ui.components.tracks :as tracks]
+            ))
 
 
 ;(defn album-artists [artists]
@@ -20,8 +22,8 @@
 ;    [:h6.card-subtitle (album-artists (:album/artists album))]]
 ;   [:.card-body "Available markets " (map (fn [m] (str m " ")) (:album/available_markets album))]])
 ;
-;(rum/defc albums [*db]
-;  (let [albums (db/get-albums *db)]
+;(rum/defc albums [db]
+;  (let [albums (db/get-albums db)]
 ;    [:.columns (map (fn [album] [:.column.col-6.col-xs-12 {:key (:db/id album)} (album-card album)]) albums)]))
 
 ;(rum/defc profile-login []
@@ -32,16 +34,16 @@
 ;(rum/defc profile [me]
 ;  [:div [:h2 (:me/display_name me)] (:me/country me)])
 ;
-;(rum/defc profile-base [*db]
-;  (let [me (db/me *db)]
+;(rum/defc profile-base [db]
+;  (let [me (db/me db)]
 ;    (utils/info me (:me/status me))
 ;    (cond
 ;      (= :logged-out (:me/status me)) (profile-login)
 ;      (= :logged-in (:me/status me)) (profile me)
 ;      (= :loading (:me/status me)) [:div "Loading..."])))
 
-(rum/defc topbar [*db]
-  (let [me (db/me *db)]
+(rum/defc topbar [db]
+  (let [me (db/me db)]
     [:section.section.section-header.bg-gray
      [:section.grid-header.container.grid-960
       [:nav.navbar
@@ -53,33 +55,47 @@
                                                   :class-name "btn btn-primary"}
                                                  "Log out")]]]]))
 
-(rum/defc sidebar [*db]
-  [:section.columns
-   [:div.trak-sidebar.column.col-3.col-sm-12 (profile/profile *db)]])
+(rum/defc sidebar [db]
+  [:div.trak-sidebar.column.col-3.col-sm-12 (profile/profile db)])
 
-(rum/defc fullpage [*db]
+(rum/defc main [db]
+  (let [current-page (:handler (db/app-state db))]
+    (cond
+      (= :playlist current-page)
+      (let [page (db/page-state db)
+            status (:application/status page)
+            title (:title (:application/state page))]
+        [:.column.col-9.col-sm-12.main
+         [:h1.text-center title]
+         (cond
+           (= status :loading) [:.loading]
+           (= status :loaded) (tracks/tracks db))]))))
+
+(rum/defc fullpage [db]
   [:section
-   (topbar *db)
+   (topbar db)
    [:section.container.grid-960
-    (sidebar *db)]])
+    [:section.columns
+     (sidebar db)
+     (main db)]]])
 
-(rum/defc app [*db]
-  (when-let [match (utils/current-path *db)]
+(rum/defc app [db]
+  (when-let [match (utils/current-path db)]
     (utils/info "Application mounted. Matched: " match)
-    (let [status (:me/status (db/me *db))]
+    (let [status (:me/status (db/me db))]
       (cond
         (= :logged-out status) (landing/landing status)
         (= :loading status) (landing/landing status)
-        (= :logged-in status) (fullpage *db)
+        (= :logged-in status) (fullpage db)
         ))
     ;[:div
-    ; (profile-base *db)
+    ; (profile-base db)
     ; ;
     ; ;(cond
-    ; ;  (empty? (db/me *db)) (components/link
+    ; ;  (empty? (db/me db)) (components/link
     ; ;                         {:href (globals/spotify-authorization-link "user-read-private user-read-email") :class-name "btn btn-primary"}
     ; ;                         "Log in")
     ; ;  :else "noi")
     ; (cond
-    ;   (= (:handler (utils/current-path *db)) :index) (albums *db))])
+    ;   (= (:handler (utils/current-path db)) :index) (albums db))])
     ))
